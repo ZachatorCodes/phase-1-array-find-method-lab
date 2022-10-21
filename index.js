5f25e4b3900e424fda852411058ca7eac2d7e646
Zach Schwartz <zachatorcodes@gmail.com>
2022-10-20 19:44:17 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(array) {
+    const value = array.find(obj => obj.result === "W");
+    if (typeof value === 'object') {
+        return value.year;
+    }
+    else {
+        return undefined;
+    }
+}
